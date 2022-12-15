b97b467061a352802d5bc845fe304070ab7d726e
Uroš Milošević <milosevicurose14@gmail.com>
2022-12-15 16:57:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Login } from "./Login";
+import logo from './logo.svg';
+import React, {useState} from 'react';
+import './App.css';
+import { Register } from './Register';
+import { Login } from './Login';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState("login");
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <Login />
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
-
 export default App;
-/*
-{currentForm === "login" ? <Login /> :   <Register />}*/
